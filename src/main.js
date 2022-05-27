dc00e4215a55f7575604d56043e22187a46952c8
Freddy Ignacio Colque <ignaciocolque99@gmail.com>
2022-05-27 17:03:50 -0300

    Creacion de lista de categorias

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-async function getTrendingMoviePreview(){
+async function getTrendingMoviesPreview(){
     const res = await fetch("https://api.themoviedb.org/3/trending/movie/day?api_key=" + API_KEY);
     const data = await res.json();
 
@@ -19,4 +19,28 @@ async function getTrendingMoviePreview(){
     });
 }
 
-getTrendingMoviePreview();
+
+async function getCategoriesPreview(){
+    const res = await fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=" + API_KEY);
+    const data = await res.json();
+
+    const categories = data.genres;
+    categories.forEach(category => {
+        const previewCategoriesContainer = document.querySelector("#categoriesPreview .categoriesPreview-list");
+
+        const categoryContainer = document.createElement("div");
+        categoryContainer.classList.add("category-container");
+
+        const categoryTitle = document.createElement("h3");
+        categoryTitle.classList.add("category-title");
+        categoryTitle.setAttribute("id", 'id' + category.id);
+        const categoryTitleText = document.createTextNode(category.name);
+
+        categoryTitle.appendChild(categoryTitleText);
+        categoryContainer.appendChild(categoryTitle);
+        previewCategoriesContainer.appendChild(categoryContainer);
+    });
+}
+
+getTrendingMoviesPreview();
+getCategoriesPreview();
